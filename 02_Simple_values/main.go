cb1f619b9ce3eeb6374c2e759702b5b81ef3712b
ErGovindthakur <ergovindthakur@gmail.com>
2025-06-24 15:14:02 +0530

    explores basics of golang

---
+++ 02_Simple_values/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main(){
+	// int
+	fmt.Println(1+2)
+
+	// float
+	fmt.Println(1.2 * 1.2)
+
+	// boolean
+	fmt.Println(false)
+	fmt.Println(true)
+
+	// string
+	fmt.Println("Hello Golang")
+}
