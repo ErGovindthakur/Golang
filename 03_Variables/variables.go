039073667033f54a621e6b123a7cec60d5b07476
ErGovindthakur <ergovindthakur@gmail.com>
2025-06-24 15:30:39 +0530

    explores how to declare variables in go

---
+++ 03_Variables/variables.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+func main(){
+
+	//var num int = 12
+
+	//var num = 12
+
+	num := 12
+	fmt.Println(num)
+
+
+	// Note -: We can do the same process with string , float , boolean
+
+	var name string
+
+	name = "Govind"
+	fmt.Println(name)
+
+	var myBol bool
+	myBol = true
+
+	fmt.Println(myBol)
+
+	var float float32
+	float = 12.44
+	fmt.Println(float)
+
+
+	myName := "Govind Thakur"
+	fmt.Println(myName)
+
+}
