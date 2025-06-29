c7c00c0d9b78966d81d92f013f81c33491b82129
ErGovindthakur <ergovindthakur@gmail.com>
2025-06-30 00:57:26 +0530

    explore go with complex function structure

--- 08_Lets_Revise_Go/Todo_App/main.go
+++ 08_Lets_Revise_Go/Todo_App/main.go
@@ -5,5 +5,32 @@ import "fmt"
 func main(){
 	fmt.Println("Welcome to our Go Todo Application !")
 
-	
+	var taskList = []string{"Code","Practice"}
+
+	fmt.Println()
+	fmt.Println("Previous Task -: ")
+	showTask(taskList)
+
+	fmt.Println()
+	taskList = addTask(taskList,"Eat")
+	taskList = addTask(taskList,"Sleep")
+
+
+	fmt.Println()
+	fmt.Println("Updated Task -: ")
+	showTask(taskList)
+
+}
+
+// 1. show all task
+func showTask(taskList[]string){
+	for index, task:=range taskList{
+		fmt.Printf("%d. %s\n",index+1,task)
+	}
+}
+
+// 2. create all task
+func addTask(taskList[]string,task string) []string{
+	var newTask = append(taskList,task)
+	return newTask
 }
